a24089d56759fe443a4caaa59efc418ba0911370
valentina-pulgarin <valentinapulgarinfernandez@medellin.edu.co>
2020-09-23 20:44:55 -0500

    maquetacion del inicio de sesión

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,12 +1,49 @@
 import React from "react";
 import NavBar from "./Navbar";
+import "./Inicio.css"
 
 export const Inicio = () => {
   return (
-    <div>
+    <div className="inicio">
       <NavBar />
-      <h1 className="text-center mt-5">Inicio</h1>
+      <div className="container">
+        <div className="titulo mt-5">
+          <h1>Iniciar sesión</h1>
+        </div>
+        <div className="entrada">
+          <div className="input-group mb-3 ">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Usuario o número*"
+            ></input>
+          </div>
+          <div className="entrada2 mt-5">
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Contraseña*"
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div className="campos">
+          <p className="font-weight-light">
+            Los campos (*) son obligatorios
+          </p>
+        </div>
+        <div className="position">
+          <button
+            type="btn"
+            className="btn btn-lg active btn-block rounded-pill text-white"
+          >
+            Iniciar
+          </button>
+        </div>
+      </div>
     </div>
+
   );
 };
 
