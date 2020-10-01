e71aae3d8c449ea62839edf4a8732765265b2494
juanfers1 <juanducas446@gmail.com>
2020-10-01 12:31:08 -0500

    Prop :recycle: del navbar

--- frontend/src/routers/Aplicacion.jsx
+++ frontend/src/routers/Aplicacion.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Inicio from "../components/Inicio";
 import Registro from "../components/Registro";
 import Bienvenido from "../components/Bienvenido"
-import consejos from "../components/consejos"
-import ayuda from "../components/ayuda"
+import Consejos from "../components/Consejos"
+import Ayuda from "../components/Ayuda"
 import App from '../App.js'
 
 function Aplicacion(){
@@ -15,8 +15,8 @@ function Aplicacion(){
           <Route exact path="/inicio" component={Inicio}/>
           <Route exact path="/registro" component={Registro}/>
           <Route exact path="/bienvenido" component={Bienvenido}/>
-          <Route exact path="/consejos" component={consejos}/>
-          <Route exact path="/ayuda" component={ayuda}/>
+          <Route exact path="/consejos" component={Consejos}/>
+          <Route exact path="/ayuda" component={Ayuda}/>
         </Switch>
         </BrowserRouter>
     )
