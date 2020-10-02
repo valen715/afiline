1a7bbc643de96c30903e0e6dbd1d81714e6ace9d
3Morales04 <3morales04@gmail.com>
2020-10-01 23:54:45 -0500

    Jola

--- frontend/src/routers/Aplicacion.jsx
+++ frontend/src/routers/Aplicacion.jsx
@@ -5,6 +5,9 @@ import Registro from "../components/Registro";
 import Bienvenido from "../components/Bienvenido"
 import Consejos from "../components/Consejos"
 import Ayuda from "../components/Ayuda"
+import CalculadorEgresos from "../components/CalculadorEgresos"
+import CalculadorIngresos from "../components/CalculadorIngresos"
+import Estadisticas from "../components/Estadisticas"
 import App from '../App.js'
 
 function Aplicacion(){
@@ -17,6 +20,10 @@ function Aplicacion(){
           <Route exact path="/bienvenido" component={Bienvenido}/>
           <Route exact path="/consejos" component={Consejos}/>
           <Route exact path="/ayuda" component={Ayuda}/>
+          <Route exact path="/calculadoregresos" component={CalculadorEgresos}/>
+          <Route exact path="/calculadoringresos" component={CalculadorIngresos}/>
+          <Route exact path="/estadisticas" component={Estadisticas}/>
+
         </Switch>
         </BrowserRouter>
     )
