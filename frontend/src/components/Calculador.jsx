90e3348ec255db343900f971a6bab9e8f348a4b1
Valentina Pulgarin <valentinapf03@gmail.com>
2020-10-02 09:49:55 -0500

    ruta3

--- frontend/src/components/Calculador.jsx
+++ frontend/src/components/Calculador.jsx
@@ -8,12 +8,15 @@ export const Calculador = () => {
             <div className="container">
                 <h1 className="text-center mt-1 ">Calculador</h1>
                 <div className="button_1 ml-4 mr-4">
-                    <button type="button" className="btn  btn-lg btn-block text-left" Style="color: #4c9954; background-color: #8bed96;">Ingresos<div className="ingresos-button">
+                <button type="button" className="btn  btn-lg btn-block text-left" Style="color: #4c9954; background-color: #8bed96;"> <Link to="/calculadoringresos"  Style="color:#4c9954">
+          Ingresos      </Link><div className="ingresos-button">
                      
-                    </div></button>
+                    </div>
+                   </button>
                 </div>
                 <div className="button_2 ml-4 mr-4 mt-5">
-                    <button type="button" className="btn  btn-lg btn-block text-left" Style="color: #4c9954; background-color: #8bed96;">Egresos <div className="ingresos-button">
+                    <button type="button" className="btn  btn-lg btn-block text-left" Style="color: #4c9954; background-color: #8bed96;"><Link to="/calculadoregresos"  Style="color:#4c9954">
+          Egresos    </Link> <div className="ingresos-button">
                         
                     </div>
                     </button>
