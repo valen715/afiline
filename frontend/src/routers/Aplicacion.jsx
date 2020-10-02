b9d2671770379e751814a71c914ce20cb4ade38f
Valentina Pulgarin <valentinapf03@gmail.com>
2020-10-02 09:17:23 -0500

    hola como te va

--- frontend/src/routers/Aplicacion.jsx
+++ frontend/src/routers/Aplicacion.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Inicio from "../components/Inicio";
 import Registro from "../components/Registro";
 import Bienvenido from "../components/Bienvenido"
-import Consejos from "../components/Consejos"
-import Ayuda from "../components/Ayuda"
+import Consejos from "../components/consejos"
+import Ayuda from "../components/ayuda"
 import CalculadorEgresos from "../components/CalculadorEgresos"
 import CalculadorIngresos from "../components/CalculadorIngresos"
 import Estadisticas from "../components/Estadisticas"
